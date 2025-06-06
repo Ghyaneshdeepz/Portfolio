0f73c4af00898772f95f3bd43e9168c6d056a426
Ghyaneshdeepz <ghyanesh.co@gmail.com>
2025-06-06 12:45:28 +0530

    padding chnages

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -14,7 +14,7 @@ const Skills = ({ className }) => {
   }, []);
 
   return (
-    <div className={`flex flex-col md:flex-row items-center gap-0 sm:gap-85 px-20 py-15 ${className}`}>
+    <div className={`flex flex-col md:flex-row items-center gap-0 sm:gap-85 px-10 py-10 sm:px-20 sm:py-15 ${className}`}>
       
       <div className="max-w-xl text-center md:text-left space-y-6">
         <h1 className="text-4xl font-bold text-black">
